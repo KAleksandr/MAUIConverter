50af706686169d599cb30484fd516db5ee5e5c1f
KAleksandr <okuzmenkoa@gmail.com>
2023-07-14 15:19:56 +0300

    Making the conversion between units

--- MauiVerter/MVVM/ViewModels/ConverterViewModel.cs
+++ MauiVerter/MVVM/ViewModels/ConverterViewModel.cs
@@ -1,17 +1,16 @@
-﻿using Amazon.S3;
-using Amazon.S3.Transfer;
-using Amazon.Translate;
-using Amazon.Translate.Model;
+﻿using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using UnitsNet;
 
 namespace MauiVerter.MVVM.ViewModels
 {
+    [AddINotifyPropertyChangedInterface]
     public class ConverterViewModel
     {
         public string QuantityName { get; set; }
@@ -19,12 +18,24 @@ namespace MauiVerter.MVVM.ViewModels
         public ObservableCollection<string> ToMeasures { get; set; }
         public string CurrentFromMeasure { get; set; }
         public string CurrentToMeasure { get; set; }
+        public double FromValue { get; set; } = 1;
+        public double ToValue { get; set; } = 1;
+        public ICommand ReturnCommand =>
+            new Command(() => Convert());
         public ConverterViewModel() {
             QuantityName = "Length";
             FromMeasures = LoadMeasures();
             ToMeasures = LoadMeasures();
-            CurrentFromMeasure = TranslateToUk("Meter");
-            CurrentToMeasure = TranslateToUk("Centimeter");
+            CurrentFromMeasure = "Meter";
+            CurrentToMeasure = "Centimeter";
+            Convert();
+        }
+        public void Convert()
+        {
+            var result = UnitConverter
+                .ConvertByName(FromValue, QuantityName, CurrentFromMeasure, CurrentToMeasure);
+            ToValue = result;
+
         }
         private ObservableCollection<string> LoadMeasures()
         {
@@ -33,56 +44,8 @@ namespace MauiVerter.MVVM.ViewModels
                 .FirstOrDefault(x => x.Name == QuantityName)
                 .UnitInfos
                 .Select(u => u.Name)
-                .ToList();
-            types.ForEach(x => x = TranslateToUk(x));
+                .ToList();           
             return new ObservableCollection<string>(types);
-        }
-        public static string TranslateToUk(string srcText)
-        {
-            return Translate(srcText).Result;
-        }
-       
-        public static string TranslateToEn(string srcText)
-        {
-            return Translate(srcText, false).Result;
-        }
-        private static async  Task<string> Translate(string srcText, bool enTouk = true)
-        {
-            var client = new AmazonTranslateClient();
-            string srcLang = "en"; // English.
-            string destLang = "uk"; // Ukraine.           
-            srcLang = enTouk ? srcLang : destLang;
-            destLang = enTouk ? destLang : srcLang;
-
-            var destText = await TranslatingTextAsync(client, srcLang, destLang, srcText);
-            return destText;
-        }
-        private static async Task<string> GetSourceTextAsync(string srcBucket, string srcTextFile)
-        {
-            string srcText = string.Empty;
-
-            var s3Client = new AmazonS3Client();
-            TransferUtility utility = new TransferUtility(s3Client);
-
-            using var stream = await utility.OpenStreamAsync(srcBucket, srcTextFile);
-
-            StreamReader file = new System.IO.StreamReader(stream);
-
-            srcText = file.ReadToEnd();
-            return srcText;
-        }
-        private static async Task<string> TranslatingTextAsync(AmazonTranslateClient client, string srcLang, string destLang, string text)
-        {
-            var request = new TranslateTextRequest
-            {
-                SourceLanguageCode = srcLang,
-                TargetLanguageCode = destLang,
-                Text = text,
-            };
-
-            var response = await client.TranslateTextAsync(request);
-
-            return response.TranslatedText;
-        }
+        }       
     }
 }
